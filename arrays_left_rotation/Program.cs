593da451a611daefde6c9d3f33567475ec4f9260
yarmolovichalex <yarmolovichalex@gmail.com>
2017-01-07 21:10:39 +0300

    Cracking the Coding Interview: Arrays: Left Rotation

---
+++ arrays_left_rotation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace arrays_left_rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parameters = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).ToList();
+            int count = parameters[0];
+            int d = parameters[1];
+
+            var data = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).ToList();
+            
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(data[(i + d) % count] + " ");
+            }
+        }
+    }
+}
