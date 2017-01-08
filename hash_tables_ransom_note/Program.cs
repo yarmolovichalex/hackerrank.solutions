c40f197ceab565990fd64784034e5c9f7772fe53
yarmolovichalex <yarmolovichalex@gmail.com>
2017-01-08 20:22:37 +0300

    Cracking the Coding Interview: Hash Tables: Ransom Note

---
+++ hash_tables_ransom_note/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace hash_tables_ransom_note
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parameters = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int m = parameters[0];
+            int n = parameters[1]; 
+            var magazineWords = Console.ReadLine().Split(' ');
+            var ransomWords = Console.ReadLine().Split(' ');
+
+            var magazineWordsDict = magazineWords.GroupBy(word => word)
+                .ToDictionary(group => group.Key, group => group.Count());
+
+            var ransomWordsDict = ransomWords.GroupBy(word => word)
+                .ToDictionary(group => group.Key, group => group.Count());
+
+            bool success = 
+                ransomWordsDict.Keys
+                .All(word => magazineWordsDict.ContainsKey(word) && magazineWordsDict[word] >= ransomWordsDict[word]);
+
+            Console.WriteLine(success ? "Yes" : "No");
+        }
+    }
+}
