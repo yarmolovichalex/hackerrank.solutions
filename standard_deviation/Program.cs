73f221b1ea923c3f9c6b7d0f79f6e272ef6e630d
yarmolovichalex <yarmolovichalex@gmail.com>
2017-01-08 19:18:34 +0300

    10 Days of Statistics: Standard Deviation

---
+++ standard_deviation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace standard_deviation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var data = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).ToList();
+
+            var standardDeviation = calculateStandardDeviation(data, n);
+            Console.WriteLine(standardDeviation.ToString("0.0"));
+        }
+
+        static double calculateStandardDeviation(List<int> data, int n)
+        {
+            var mean = calculateMean(data);
+            var sumOfSquaredDistances = data.Aggregate(0.0, (seed, item) => seed += Math.Pow(item - mean, 2));
+            var standardDeviation = Math.Sqrt(sumOfSquaredDistances/n);
+
+            return standardDeviation;
+        }
+
+        static double calculateMean(ICollection<int> seq)
+        {
+            var result = (double)seq.Sum() / seq.Count;
+            return result;
+        }
+    }
+}
