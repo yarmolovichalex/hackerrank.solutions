a76ad4c5a469cc230171f06be52e7d1293425598
yarmolovichalex <yarmolovichalex@gmail.com>
2017-01-08 20:30:56 +0300

    move the project to different folder

--- mean_median_and_mode/Properties/AssemblyInfo.cs
+++ mean_median_and_mode/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hackerrank.solutions.mean_median_and_mode")]
+[assembly: AssemblyTitle("mean_median_and_mode")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("hackerrank.solutions.mean_median_and_mode")]
+[assembly: AssemblyProduct("mean_median_and_mode")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
