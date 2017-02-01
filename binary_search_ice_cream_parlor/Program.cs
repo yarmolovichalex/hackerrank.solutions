3816161c0274a58d86207ac509534c7262157bca
yarmolovichalex <yarmolovichalex@gmail.com>
2017-02-02 02:07:22 +0300

    Cracking the Coding Interview: Binary Search: Ice Cream Parlor

---
+++ binary_search_ice_cream_parlor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace binary_search_ice_cream_parlor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                int money = Convert.ToInt32(Console.ReadLine());
+                int numbersOfFlavors = Convert.ToInt32(Console.ReadLine());
+                int[] flavors = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+                var map = new Dictionary<int, int>();
+                for (int j = 0; j < numbersOfFlavors; j++)
+                {
+                    int complement = money - flavors[j];
+                    if (map.ContainsKey(complement))
+                    {
+                        var listOfKeys = new List<int> { j + 1, map[complement] }
+                            .OrderBy(l => l)
+                            .ToList();
+
+                        Console.WriteLine($"{listOfKeys[0]} {listOfKeys[1]}");
+                        break;
+                    }
+
+                    if (!map.ContainsKey(flavors[j]))
+                        map.Add(flavors[j], j + 1);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
