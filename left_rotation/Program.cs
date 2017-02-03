a35ddf520f681cbb0d4273c98c1ab117b62bcf66
yarmolovichalex <yarmolovichalex@gmail.com>
2017-02-03 13:50:47 +0300

    Data Structures: Arrays: Left Rotation

---
+++ left_rotation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace left_rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parameters = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int n = parameters[0];
+            int k = parameters[1];
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = arr[(i + k) % n];
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
