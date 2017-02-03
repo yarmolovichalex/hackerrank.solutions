d7c99aa4e0b15b4dbe3ea222b3c41d9c0cbf1447
yarmolovichalex <yarmolovichalex@gmail.com>
2017-02-03 14:52:35 +0300

    Data Structures: Arrays: Algorithmic Crush

---
+++ algorithmic_crush/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace algorithmic_crush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] parameters = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+            long n = parameters[0];
+            long m = parameters[1];
+
+            long[] arr = new long[n];
+            for (long i = 0; i < m; i++)
+            {
+                long[] data = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+                long a = data[0];
+                long b = data[1];
+                long sum = data[2];
+
+                arr[a - 1] += sum;
+                if (b < n)
+                    arr[b] -= sum;
+            }
+
+            long temp = 0;
+            long max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                temp += arr[i];
+                if (max < temp)
+                    max = temp;
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
