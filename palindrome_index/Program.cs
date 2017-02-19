0b5c390a358f4942e2455b5a289a4027b20b38a6
Alexander Yarmolovich <yarmolovichalex@gmail.com>
2017-02-19 20:05:35 +0300

    Algorithms: Strings: Palindrome Index

---
+++ palindrome_index/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace palindrome_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+
+                int length = word.Length;
+                bool found = false;
+                for (int j = 0; j < length; j++)
+                {
+                    if (word[j] != word[length - j - 1])
+                    {
+                        if (word[j + 1] == word[length - j - 1] 
+                            && word[j + 2] == word[length - j - 2])
+                        {
+                            Console.WriteLine(j);
+                        }
+                        else
+                        {
+                            Console.WriteLine(length - j - 1);
+                        }
+
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                    Console.WriteLine(-1);
+            }
+        }
+    }
+}
