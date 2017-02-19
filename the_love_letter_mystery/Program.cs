ec4eba864e8f904dec831fe590ebefdbbe0a4956
Alexander Yarmolovich <yarmolovichalex@gmail.com>
2017-02-19 19:35:18 +0300

    Algorithms: Strings: The Love-Letter Mystery

---
+++ the_love_letter_mystery/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace the_love_letter_mystery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+                int result = 0;
+
+                int length = word.Length;
+                for (int j = 0; j < length/2; j++)
+                {
+                    result += Math.Abs(word[j] - word[length - j - 1]);
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
