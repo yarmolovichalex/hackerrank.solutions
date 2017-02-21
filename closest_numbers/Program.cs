69d48788d5270ec8ee64d9fabcc380880e1e65c4
Alexander Yarmolovich <yarmolovichalex@gmail.com>
2017-02-21 17:19:24 +0300

    Algorithms: Sorting: Closest Numbers

---
+++ closest_numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace closest_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Array.Sort(numbers);
+
+            int minDiff = int.MaxValue;
+            List<int> pairs = new List<int>();
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int diff = numbers[i] - numbers[i - 1];
+                if (minDiff > diff)
+                {
+                    minDiff = diff;
+                    pairs.Clear();
+                    pairs.Add(numbers[i]);
+                    pairs.Add(numbers[i - 1]);
+                }
+                else if (minDiff == diff)
+                {
+                    pairs.Add(numbers[i]);
+                    pairs.Add(numbers[i - 1]);
+                }
+            }
+
+            string result = string.Join(" ", pairs.OrderBy(p => p));
+            Console.WriteLine(result);
+        }
+    }
+}
